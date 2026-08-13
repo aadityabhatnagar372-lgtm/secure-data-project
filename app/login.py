ac0e176d8f3850dfb8f0eecec9c497203186b5fb
Aaditya Bhatnagar <aadityabhatnagar372@gmail.com>
2026-08-13 12:00:51 +0530

    Add login endpoint structure

--- app/login.py
+++ app/login.py
@@ -1,6 +1,18 @@
+from fastapi import APIRouter
 from pydantic import BaseModel
+
+
+router = APIRouter()
 
 
 class LoginRequest(BaseModel):
     username: str
     password: str
+
+
+@router.post("/login")
+def login(request: LoginRequest):
+    return {
+        "message": "Login endpoint reached",
+        "username": request.username,
+    }
