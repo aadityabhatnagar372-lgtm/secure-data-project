1628432b2d08343787933a3b1c4b8ee5084a8328
Aaditya Bhatnagar <aadityabhatnagar372@gmail.com>
2026-08-13 12:03:30 +0530

    Connect login router to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
+from app.login import router as login_router
 from app.routes import router
 
 app = FastAPI(title="Secure Distributed Data Access System")
 
 app.include_router(router)
+app.include_router(login_router)
 
 
 @app.get("/")
