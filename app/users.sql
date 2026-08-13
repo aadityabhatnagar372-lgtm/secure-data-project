008608461ac30dc403cb3f43c25bfc67669f2586
Aaditya Bhatnagar <aadityabhatnagar372@gmail.com>
2026-08-13 13:21:38 +0530

    Add customer ownership to users

--- app/users.sql
+++ app/users.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL
+    password_hash TEXT NOT NULL,
+    customer_id INTEGER REFERENCES customers(id)
 );
